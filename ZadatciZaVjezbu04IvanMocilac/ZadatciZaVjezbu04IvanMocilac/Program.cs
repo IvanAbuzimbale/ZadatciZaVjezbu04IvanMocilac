33b62e740ada7947532087b36b590c5bf7f56fc5
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-12-11 18:00:35 +0100

    Add files via upload

---
+++ ZadatciZaVjezbu04IvanMocilac/ZadatciZaVjezbu04IvanMocilac/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatciZaVjezbu04IvanMocilac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 0, 9, 16, 7, 5, 17, 4, 14, 39, 1 };
+            Sort.QuickSort(arr, 0, arr.Length - 1);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ZadatciZaVjezbu04IvanMocilac/ZadatciZaVjezbu04IvanMocilac/Sort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatciZaVjezbu04IvanMocilac
+{
+    class Sort
+    {
+        public static void QuickSort(int[] arr, int left, int right)
+        {
+            int l = left;
+            int r = right;
+            int s = arr[(left + right) / 2];
+
+            do
+            {
+                while (arr[l] > s && l < right)
+                {
+                    l++;
+                }
+                while (arr[r] < s && r > left)
+                {
+                    r--;
+                }
+                if (l <= r)
+                {
+                    int a = arr[l];
+                    arr[l] = arr[r];
+                    arr[r] = a;
+                    l++;
+                    r--;
+                }
+            } while (l <= r);
+
+            if (left < r)
+            {
+                QuickSort(arr, left, r);
+            }
+
+            if (l < right)
+            {
+                QuickSort(arr, l, right);
+            }
+        }
+    }
+}
